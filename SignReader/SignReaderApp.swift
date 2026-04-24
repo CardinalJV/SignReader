f81069eba9683f8c4dd55ae300e661e9bec5d0b8
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2026-04-24 16:54:44 +0200

    Initial Commit

---
+++ SignReader/SignReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignReaderApp.swift
+//  SignReader
+//
+//  Created by Viranaiken Jessy on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SignReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
